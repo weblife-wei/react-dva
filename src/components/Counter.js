c533450ca4b460b54d351e1daeef1280327c3752
wtz <wtz852456@163.com>
2020-03-15 15:55:14 +0800

    routerRedux路由跳转

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from "dva"
 import PropTypes from "prop-types"   //dva中自带了prop-types校验库
+import {routerRedux} from "dva/router"
 
 function Counter(props) {
   console.log('counter-props',props)  //{history,location,match,dispatch,counter}
@@ -11,6 +12,8 @@ function Counter(props) {
        <br/>
        <button onClick={()=>dispatch({type:'counter/add'})}>+</button>   {/* 发送action的时候必须将models的nameSpace名称带上*/}
        <button onClick={()=>dispatch({type:'counter/asyncAdd'})}>异步+</button>   
+
+       <button onClick={()=>dispatch(routerRedux.push('/'))}>跳转回首页</button>
     </div>
   )
 }
